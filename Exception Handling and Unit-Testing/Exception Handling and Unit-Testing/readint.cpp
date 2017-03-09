bb515b9b24025b5566fd23927e9d8821a5f001fd
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2017-03-08 17:19:17 -0800

    -Added initial information provided in assignment.

---
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.cpp
@@ -0,0 +1,17 @@
+#include "readint.h"
+#include <stdexcept>
+using namespace std;
+
+///////////////////////
+// Ben Morledge-Hampton
+// 
+// Exception Handling and Unit-Testing
+//
+// 3/8/2017
+///////////////////////
+
+int read_int(const string &prompt, int low, int high) {
+
+
+
+}
---
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.h
@@ -0,0 +1,12 @@
+#ifndef READINT_H_
+#define READINT_H_
+#include <string>
+
+/**
+ 
+
+
+*/
+int read_int(const std::string &prompt, int low, int high);
+
+#endif
