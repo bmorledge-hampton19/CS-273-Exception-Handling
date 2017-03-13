eef006a90d2f8ebe34e1f705d107afe0074a1237
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2017-03-12 20:04:15 -0700

    -Lots of stuff being committed after I finally fixed some errors with committing.

--- Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.cpp
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.cpp
@@ -10,6 +10,16 @@ using namespace std;
 // 3/8/2017
 ///////////////////////
 
+/**
+Function to return an integer as it is input by the user.
+However, the program will only return the integer if it
+falls within the given bounds.  If a non-number is entered,
+the function will discard the invalid input, and reprompt for input.
+@param prompt The message displayed to the user to prompt input.
+@param low The first integer on the lower bound which will be considered invalid.
+@param high The first integer on the upper bound which will be considered invalid.
+@Return the user's valid input.
+*/
 int read_int(const string &prompt, int low, int high) {
 
 
--- Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.h
+++ Exception Handling and Unit-Testing/Exception Handling and Unit-Testing/readint.h
@@ -3,9 +3,14 @@
 #include <string>
 
 /**
- 
-
-
+ Function to return an integer as it is input by the user.
+ However, the program will only return the integer if it
+ falls within the given bounds.  If a non-number is entered,
+ the function will discard the invalid input, and reprompt for input.
+ @param prompt The message displayed to the user to prompt input.
+ @param low The first integer on the lower bound which will be considered invalid.
+ @param high The first integer on the upper bound which will be considered invalid.
+ @Return the user's valid input.
 */
 int read_int(const std::string &prompt, int low, int high);
 
